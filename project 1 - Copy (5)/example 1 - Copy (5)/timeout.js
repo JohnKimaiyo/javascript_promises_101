df652c089c6e8269df5c319e5210c54133cd33ee
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-02 08:04:23 -0800

    timeout to delay get user lofic

---
+++ project 1 - Copy (5)/example 1 - Copy (5)/timeout.js
@@ -0,0 +1,17 @@
+function getUsers(callback) {
+    setTimeout(() => {
+        callback([
+            { username: "john", email: "john@gmail.com" },
+            { username: "jane", email: "jane@gmail.com" }
+        ]);
+    }, 1000);
+}
+
+function findUser(username, callback) {
+    getUsers((users) => {
+        const user = users.find((user) => user.username === username);
+        callback(user);
+    });
+}
+
+findUser('john', console.log);
