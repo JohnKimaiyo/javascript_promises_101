0801b5e6397107e9a7669611cbc213d633f0916c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-02 11:48:46 -0800

    retunr list of users

---
+++ project 1 - Copy (10)/users.js
@@ -0,0 +1,9 @@
+// to retunr a a list of users
+function getUsers(){
+    console.log([
+        {username:"john",email:"john@gmail.com"},
+        {username:"jane",email:"jane@gmail.com"}
+    ])
+}
+
+getUsers();
