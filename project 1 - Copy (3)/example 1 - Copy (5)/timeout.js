2034a99a5d4f07eb89f5f8de36d744ae74346db4
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-01 23:30:56 -0800

    setimout to get user logic

---
+++ project 1 - Copy (3)/example 1 - Copy (5)/timeout.js
@@ -0,0 +1,18 @@
+// delay get get user request
+function getUsers(callback) {
+    setTimeout(() => {
+        callback([
+            { username: "john", email: "john@gmail.com" },
+            { username: "jane", email: "jane@gmail.com" }
+        ]);
+    }, 1000);
+}
+
+function findUser(username, callback) {
+    getUsers((users) => {
+        const user = users.find((user) => user.username === username);
+        callback(user);
+    });
+}
+
+findUser('john', console.log);
