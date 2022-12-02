9db166ea3625adfe2f3cb6b9c8700d9cb0bbaefe
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-02 04:53:28 -0800

    timeout get user request

---
+++ project 1 - Copy (5)/example 1 - Copy (5)/users.js
@@ -0,0 +1,17 @@
+// to return list of users
+function getUsers() {
+    return [
+        { username: "john", email: "john@gmail.com" },
+        { username: "jane", email: 'jane@gmail.com' }
+    ]
+}
+
+
+// to find specifi user
+function findUser(username) {
+    const users = getUsers();
+    const user = users.find((user) => user.username === username);
+    return user;
+}
+
+console.log(findUser('john'));
