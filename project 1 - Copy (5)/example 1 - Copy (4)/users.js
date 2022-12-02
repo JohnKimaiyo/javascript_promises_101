33b3f8e7154e512f889acae083900091c3088e2a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-02 04:26:21 -0800

    get users loic

---
+++ project 1 - Copy (5)/example 1 - Copy (4)/users.js
@@ -0,0 +1,16 @@
+// to return a list users
+function getUsers() {
+    return [
+        { username: "john", email: "john@gmail.com" },
+        { username: "jane", email: "jane@gmail.com" }
+    ]
+}
+
+// to find a specific user
+function findUser(username) {
+    const users = getUsers();
+    const user = users.find((user) => user, username === username);
+    return user;
+}
+
+console.log(findUser('john'));
