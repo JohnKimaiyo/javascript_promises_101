37c45fa16d0dde7044bfbe0061be029e91e6bc37
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-04 08:12:23 -0800

    promise created

---
+++ promise 1 - Copy (8)/app.js
@@ -0,0 +1,22 @@
+function myDisplayer(some) {
+  console.log(some);
+}
+
+let myPromise = new Promise(function (myResolve, myReject) {
+  let x = 0;
+
+  if (x == 0) {
+    myResolve("ok");
+  } else {
+    myReject("error");
+  }
+});
+
+myPromise.then(
+  function (value) {
+    myDisplayer(value);
+  },
+  function (error) {
+    myDisplayer(error);
+  }
+);
