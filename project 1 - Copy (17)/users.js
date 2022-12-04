6488068cb5b7961eb1124c5be2ce25170ebde828
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-04 05:10:55 -0800

    get user logic

---
+++ project 1 - Copy (17)/users.js
@@ -0,0 +1,16 @@
+// to return a list of users
+function getUsers() {
+    return [
+        { username: "john", email: "john@gmail.com" },
+        { username: "jane", email: "jane@gmqail.com" }
+    ]
+}
+
+// to find specidic user
+function findUser(username) {
+    const users = getUsers();
+    const user = users.find((user) => user, username === username);
+    return user;
+}
+
+console.log(findUser('john'));
